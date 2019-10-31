41975339c1e7bb4a98f231568143b0008516caee
WyriHaximus <WyriHaximus@users.noreply.github.com>
2019-10-31 04:08:51 +0000

    Auto-committed changes by create-pull-request action

--- src/versions.php
+++ src/versions.php
@@ -2,6 +2,6 @@
 
 namespace WyriHaximus\FakePHPVersion;
 
-const FUTURE = '8.40.50';
-const CURRENT = '7.31.58';
-const ACTUAL = '7.3.10';
+const FUTURE = '8.55.62';
+const CURRENT = '7.43.42';
+const ACTUAL = '7.3.11';
