5cc318470ebbc4c3a67ed441993ca58d8fc03d23
WyriHaximus <147145+WyriHaximus@users.noreply.github.com>
2026-01-13 21:53:05 +0000

    [create-pull-request] automated change

--- src/versions.php
+++ src/versions.php
@@ -2,6 +2,6 @@
 
 namespace WyriHaximus\FakePHPVersion;
 
-const FUTURE = '9.688.665';
-const CURRENT = '8.738.737';
-const ACTUAL = '8.5.1';
+const FUTURE = '9.698.696';
+const CURRENT = '8.758.746';
+const ACTUAL = '8.5.2';
